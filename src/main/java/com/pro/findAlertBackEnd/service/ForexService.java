9554f00fe1d43d05afe3930078bf1cccac1c1716
trongtai <trongtai.nguyen789@gmail.com>
2025-11-12 16:40:58 +0700

    update

--- src/main/java/com/pro/findAlertBackEnd/service/ForexService.java
+++ src/main/java/com/pro/findAlertBackEnd/service/ForexService.java
@@ -5,37 +5,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pro.findAlertBackEnd.model.response.EventDetailResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @Service
 @Slf4j
 public class ForexService {
-    private static final String BASE_URL = "https://www.forexfactory.com";
-
     private final WebClient webClient;
 
     private final ObjectMapper objectMapper = new ObjectMapper(); // Jackson
 
+    private static final String SCRAPER_API_KEY = "d6d4bf0c0685e24e605e2362ee1c9f55";
+
+
     public ForexService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder
-                .baseUrl(BASE_URL)
-                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-                .build();
+        this.webClient = webClientBuilder.build();
     }
 
     @Cacheable(value = "detailsEvent", key = "#eventId")
-    public Mono<EventDetailResponse> detailsEvent(Long eventId) throws UnknownHostException {
+    public Mono<EventDetailResponse> detailsEvent(Long eventId) {
+        String scraperUrl = "https://api.scraperapi.com/?api_key=" + SCRAPER_API_KEY +
+                "&url=https://www.forexfactory.com/calendar/details/1-" + eventId +
+                "&render=true";
         return webClient
                 .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/calendar/details/1-{eventId}")
-                        .build(eventId))
+                .uri(scraperUrl)
                 .retrieve()
                 .bodyToMono(String.class)
                 .map(jsonString -> {
