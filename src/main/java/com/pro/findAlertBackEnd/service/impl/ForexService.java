1736172c8d25478fd0a9a2ef90e778ef65157331
trongtai <trongtai.nguyen789@gmail.com>
2025-12-06 16:36:27 +0700

    update

--- src/main/java/com/pro/findAlertBackEnd/service/impl/ForexService.java
+++ src/main/java/com/pro/findAlertBackEnd/service/impl/ForexService.java
@@ -10,11 +10,7 @@ import com.pro.findAlertBackEnd.service.IForexService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.io.BufferedReader;
@@ -24,78 +20,83 @@ import java.io.InputStreamReader;
 @Service
 @Slf4j
 public class ForexService implements IForexService {
-    private final WebClient webClient;
-
-    @Value("${scraper.api.key}")
-    private String SCRAPER_API_KEY;
-
-    @Value("${scraper.url}")
-    private String SCRAPER_URL;
-
     @Value("${forex.url}")
     private String FOREX_URL;
-
-    public ForexService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.exchangeStrategies(ExchangeStrategies.builder()
-                        .codecs(configurer -> configurer
-                                .defaultCodecs()
-                                .maxInMemorySize(10 * 1024 * 1024))
-                        .build())
-                .build();
-    }
 
     @Cacheable(value = "detailsEvent", key = "#eventId")
     @Override
     public Mono<DetailsEventResponse> getCalendarDetailsEvent(Long eventId) {
-//        String url = SCRAPER_URL + "/?api_key=" + SCRAPER_API_KEY +
-//                "&url=" + FOREX_URL + "/calendar/details/1-" + eventId;
         String url = FOREX_URL + "/calendar/details/1-" + eventId;
-        return webClient
-                .get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(DetailsEventResponse.class)
-                .onErrorResume(e -> {
-                    log.error("Failed to fetch DetailsEventResponse: ", e);
-                    return Mono.just(new DetailsEventResponse());
-                });
+        return Mono.fromCallable(() -> {
+            String[] cmd = {
+                    "curl",
+                    "-s",
+                    "-L",
+                    url,
+                    "-H", "User-Agent: PostmanRuntime/7.49.1",
+                    "-H", "Host: www.forexfactory.com",
+                    "-H", "Cookie: __cf_bm=dGTo1wbZI7i4DavDYmEAWqu0pS1R57VJ4nGQdh2mXCc-1765009280-1.0.1.1-fy8CTOUFIQmjXNzzVRlMF0pHj__sp4MPBf8Vv3M7h8WS9koDY.HFDPmNI.xlOstSa29hHrU6ZS7ZDB1CggUpWZEK.wt.GqhWpQfY6mPC7nDXcqYH_Heqr1uO79kRKd3t; auth_user=3805da2f8c48d8002b042cdc848ef86a47ce9e7e13623d29833e68371e4a9e0d%3A7f28c890eec9a70cf1a3f66d6e33669678b39fd7b58969d18700c85ada122cb4; fflastactivity=0; fflastvisit=1765008586; ffsettingshash=49307cf48d8eb6bfb25e963ef7c0e045; fftab-history=calendar"
+            };
+            String json = execCurl(cmd);
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(json, DetailsEventResponse.class);
+        }).onErrorResume(e -> {
+            log.error("Failed to fetch DetailsEventResponse via CURL:", e);
+            return Mono.just(new DetailsEventResponse());
+        });
     }
+
 
     @Cacheable(value = "applySettings", key = "#request")
     @Override
     public Mono<ApplySettingsResponse> getApplySettings(ApplySettingsRequest request) {
-//        String url = SCRAPER_URL + "/?api_key=" + SCRAPER_API_KEY +
-//                "&url=" + FOREX_URL + "/calendar/apply-settings/100000?navigation=1";
         String url = FOREX_URL + "/calendar/apply-settings/100000?navigation=1";
-        return webClient
-                .post()
-                .uri(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(ApplySettingsResponse.class)
-                .onErrorResume(e -> {
-                    log.error("Failed to fetch ApplySettingsResponse: ", e);
-                    return Mono.just(new ApplySettingsResponse());
-                });
+        return Mono.fromCallable(() -> {
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonRequest = mapper.writeValueAsString(request);
+            String[] cmd = {
+                    "curl",
+                    "-s",
+                    "-L",
+                    "-X", "POST",
+                    url,
+                    "-H", "User-Agent: PostmanRuntime/7.49.1",
+                    "-H", "Host: www.forexfactory.com",
+                    "-H", "Content-Type: application/json",
+                    "-H", "Cookie: __cf_bm=dGTo1wbZI7i4DavDYmEAWqu0pS1R57VJ4nGQdh2mXCc-1765009280-1.0.1.1-fy8CTOUFIQmjXNzzVRlMF0pHj__sp4MPBf8Vv3M7h8WS9koDY.HFDPmNI.xlOstSa29hHrU6ZS7ZDB1CggUpWZEK.wt.GqhWpQfY6mPC7nDXcqYH_Heqr1uO79kRKd3t; auth_user=3805da2f8c48d8002b042cdc848ef86a47ce9e7e13623d29833e68371e4a9e0d%3A7f28c890eec9a70cf1a3f66d6e33669678b39fd7b58969d18700c85ada122cb4; fflastactivity=0; fflastvisit=1765008586; ffsettingshash=49307cf48d8eb6bfb25e963ef7c0e045; fftab-history=calendar",
+                    "-d", jsonRequest
+            };
+            String jsonResponse = execCurl(cmd);
+            return mapper.readValue(jsonResponse, ApplySettingsResponse.class);
+        }).onErrorResume(e -> {
+            log.error("Failed to fetch ApplySettingsResponse via CURL:", e);
+            return Mono.just(new ApplySettingsResponse());
+        });
     }
 
     @Cacheable(value = "historyEvent", key = "#eventId + '-' + #page")
     @Override
     public Mono<HistoryEventResponse> getHistoryEvent(Long eventId, Integer page) {
-//        String url = SCRAPER_URL + "/?api_key=" + SCRAPER_API_KEY +
-//                "&url=" + FOREX_URL + "/calendar/history/1-" + eventId + "?i=" + page;
         String url = FOREX_URL + "/calendar/history/1-" + eventId + "?i=" + page;
-        return webClient
-                .post()
-                .uri(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(HistoryEventResponse.class)
-                .onErrorResume(e -> {
-                    log.error("Failed to fetch HistoryEventResponse", e);
-                    return Mono.just(new HistoryEventResponse());
-                });
+        return Mono.fromCallable(() -> {
+            String[] cmd = {
+                    "curl",
+                    "-s",
+                    "-L",
+                    "-X", "POST",
+                    url,
+                    "-H", "User-Agent: PostmanRuntime/7.49.1",
+                    "-H", "Host: www.forexfactory.com",
+                    "-H", "Content-Type: application/json",
+                    "-H", "Cookie: __cf_bm=dGTo1wbZI7i4DavDYmEAWqu0pS1R57VJ4nGQdh2mXCc-1765009280-1.0.1.1-fy8CTOUFIQmjXNzzVRlMF0pHj__sp4MPBf8Vv3M7h8WS9koDY.HFDPmNI.xlOstSa29hHrU6ZS7ZDB1CggUpWZEK.wt.GqhWpQfY6mPC7nDXcqYH_Heqr1uO79kRKd3t; auth_user=3805da2f8c48d8002b042cdc848ef86a47ce9e7e13623d29833e68371e4a9e0d%3A7f28c890eec9a70cf1a3f66d6e33669678b39fd7b58969d18700c85ada122cb4; fflastactivity=0; fflastvisit=1765008586; ffsettingshash=49307cf48d8eb6bfb25e963ef7c0e045; fftab-history=calendar"
+            };
+            String json = execCurl(cmd);
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(json, HistoryEventResponse.class);
+        }).onErrorResume(e -> {
+            log.error("Failed to fetch HistoryEventResponse via CURL:", e);
+            return Mono.just(new HistoryEventResponse());
+        });
     }
 
     @Override
@@ -124,17 +125,13 @@ public class ForexService implements IForexService {
     public String execCurl(String[] command) throws IOException {
         ProcessBuilder pb = new ProcessBuilder(command);
         pb.redirectErrorStream(true);
-
         Process process = pb.start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
         StringBuilder output = new StringBuilder();
         String line;
-
         while ((line = reader.readLine()) != null) {
             output.append(line);
         }
-
         return output.toString();
     }
 
